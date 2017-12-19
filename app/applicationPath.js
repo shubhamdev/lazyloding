ebbce742292b4658a338f43fd07868199aa034f8
shubhamdev <shubham1838@gmail.com>
2017-12-19 21:55:55 +0530

    added missing files

--- app/applicationPath.js
+++ app/applicationPath.js
@@ -1,9 +1,9 @@
-var config = false, baseUrl = "https://infiniti.medibuddy.in/static/";
+var config = false, baseUrl = "https://medibuddy.in/static/";
 
 var timeStamp = "bust=" + (new Date()).getTime(),
  modulesPaths = {
-   angular: baseUrl+ 'vendors/bower_components/angular/angular-1.5.0',
-   angularUiRouter: baseUrl + 'vendors/bower_components/angular-ui-router/release/angular-ui-router-0.2.15.min',
+   angular: 'https://www.medibuddy.in/static/vendors/bower_components/angular/angular-1.5.0.min',
+   angularUiRouter: 'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/1.0.3/angular-ui-router.min',
    bootstrap: 'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/2.5.0/ui-bootstrap.min',
    tpls: 'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/2.5.0/ui-bootstrap-tpls.min.js',
    underscore:'https://plus.medibuddy.in/thirdparty/assets/vendors/bower_components/underscore/underscore-1.6.0.min',
